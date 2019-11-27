ad9a089ff7d07e0f8a287f9bc282d94f69a93014
Edgardo610 <Edgardo610@users.noreply.github.com>
2019-11-27 14:19:52 +0100

    Try 1

--- processeur.py
+++ processeur.py
@@ -1,2 +1,14 @@
 if __name__ == '__main__':
     print("walah c'est chaud")
+
+
+class PipeLine:
+
+    def __init__(self, size):
+        self.size = size
+
+
+class Registre:
+
+    def __init__(self, who):
+        self.who = who
