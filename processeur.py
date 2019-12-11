b0c9a7ed654eece5a9d73fcad39b103431a091e5
Edgardo610 <Edgardo610@users.noreply.github.com>
2019-12-11 16:58:14 +0100

    A simple update, of my work

--- processeur.py
+++ processeur.py
@@ -12,19 +12,19 @@ import time
 
 # GLOBAL VARIABLE
 REGISTER_NAME = ["R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9"]
-CYCLES = {"LOAD": [1, 2, 1, 1],
-          "STORE": [1, 1, 1, 1],
-          "MOVE": [1, 1, 1, 1],
-          "MVC": [1, 1, 1, 1],
-          "IADD": [1, 1, 1, 1],
-          "IMUL": [3, 1, 1, 1]}
+CYCLES = {"LOAD": [1, 2, 1],
+          "STORE": [1, 1, 1],
+          "MOVE": [1, 1, 1],
+          "MVC": [1, 1, 1],
+          "IADD": [1, 1, 1],
+          "IMUL": [3, 1, 1]}
 
 def initialise():
     registres = [0]*10
     memories = [0]*16
     pipelines = []
     for _ in range(2):
-        pipelines.append([-1]*6)
+        pipelines.append([-1]*5)
     return registres, memories, pipelines
 
 
@@ -41,13 +41,25 @@ def printState(c, p1, p2, Reg, Mem):
     print(Mem)
 
 
-def move(pip):
-    pass
+def move(pip, deco, prog):
+    for i in range(2):
+        for j in range(4, 1, -1):
+            if j != 4 and pip[i][j] != -1 and decoded[pip[i][j]][j-2] == 0:
+                pip[i][j+1] = pip[i][j]
+            pip[i][j] = -1
+        if pip[i][1] != -1:
+            used = prog[pip[i][1]].split(" ")[1:]
+            if set(pip[1][2:]) == {-1}:
+                pip[i][2] = pip[i][1]
+                pip[i][1] = -1
+        if pip[i][0] != -1 and pip[i][1] == -1:
+            pip[i][1] = pip[i][0]
+            pip[i][0] = -1
+    return pip
 
 
 def fetch(pip, what):
-    pip.insert(0, what)
-    pip.pop(len(pip)-1)
+    pip[0] = what
     return pip
 
 
@@ -71,7 +83,7 @@ def memory(what, mem, rg, inst):
     if what:
         return mem[int(inst[2])]
     else:
-        mem[int(inst[1])] = registres[REGISTER_NAME.index(rg)]
+        mem[int(inst[1])] = rg[REGISTER_NAME.index(inst(2))]
         return mem
 
 
@@ -99,10 +111,12 @@ if __name__ == '__main__':
         # simulateur(registres, memoires, pipelines, prog, count, cursor_prog)
 
         for i in range(2):  # FETCH
-            print(len(prog))
             if cursor_prog < len(prog)-1 and pipelines[i][0] == -1:
                 pipelines[i] = fetch(pipelines[i], cursor_prog)
                 cursor_prog += 1
+
+        count += 1
+        printState(count, pipelines[0], pipelines[1], registres, memoires)
 
         for i in range(2):  # DECODE
             if pipelines[i][1] != -1:
@@ -118,16 +132,16 @@ if __name__ == '__main__':
                 decoded[pipelines[i][2]][0] -= 1
 
         for i in range(2):  # MEM
-            if pipelines[i][3] in decoded and decoded[pipelines[i][3]][0] > 0:
+            if pipelines[i][3] in decoded and decoded[pipelines[i][3]][1] > 0:
                 instruct = prog[pipelines[i][3]].split(" ")
                 if instruct[0] == "LOAD" and decoded[pipelines[i][3]][0] == 2:
                     value[i] = memory(True, memoires, registres, instruct)
                 elif instruct[0] == "STORE":
-                    memoires = memory(True, memoires, registres, instruct)
-                decoded[pipelines[i][3]][0] -= 1
+                    memoires = memory(False, memoires, registres, instruct)
+                decoded[pipelines[i][3]][1] -= 1
 
         for i in range(2):  # WB:
-            if pipelines[i][4] in decoded and decoded[pipelines[i][4]][0] > 0:
+            if pipelines[i][4] in decoded and decoded[pipelines[i][4]][2] > 0:
                 instruct = prog[pipelines[i][4]].split(" ")
                 if instruct[0] != "STORE":
                     if value[i] != "-1":
@@ -135,14 +149,10 @@ if __name__ == '__main__':
                     else:
                         registres[REGISTER_NAME.index(instruct[1])] = int(instruct[2])
                     value[i] = "-1"
-                decoded[pipelines[i][4]][0] -= 1
+                decoded[pipelines[i][4]][2] -= 1
 
-        count += 1
-        printState(count, pipelines[0], pipelines[1], registres, memoires)
-        for d in decoded:
-            print(str(d) + "  =>  " + str(decoded[d]))
+        print()
 
-        for i in range(2):
-            pipelines[i] = move(pipelines)
+        pipelines = move(pipelines, decoded, prog)
 
-        time.sleep(5)
+        time.sleep(0.5)
