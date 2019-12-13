6e25a20a195a261a52075be3de601311fc5caae5
Edgardo610 <Edgardo610@users.noreply.github.com>
2019-12-13 16:09:21 +0100

    THIS SHIT WORK closeeeeeeeeeee

--- processeur.py
+++ processeur.py
@@ -19,19 +19,32 @@ CYCLES = {"LOAD": [1, 2, 1],
           "IADD": [1, 1, 1],
           "IMUL": [3, 1, 1]}
 
-def initialise():
-    rg = [0]*10
-    memories = [0]*16
+
+def initialise(name):
+    """
+
+    :return:
+    """
+    rg = [0] * 10
+    memories = [0] * 16
     pip = []
-    pr = []
     for _ in range(2):
-        pip.append([-1]*5)
-    with open(file_name) as prog_file:
+        pip.append([-1] * 5)
+    with open(name) as prog_file:
         pr = prog_file.read().split("\n")
-    return rg, memories, pip, pr
+        return rg, memories, pip, pr
 
 
 def printState(c, p1, p2, Reg, Mem):
+    """
+
+    :param c:
+    :param p1:
+    :param p2:
+    :param Reg:
+    :param Mem:
+    :return:
+    """
     print("Cycle:")
     print(c)
     print("Pipeline 1:")
@@ -44,24 +57,50 @@ def printState(c, p1, p2, Reg, Mem):
     print(Mem)
 
 
-def move(pip, deco, prog):
+def move(pip, deco, pr):
+    """
+
+    :param pip:
+    :param deco:
+    :param pr:
+    :return:
+    """
     for i in range(2):
         for j in range(4, 1, -1):
-            if j != 4 and pip[i][j] != -1 and decoded[pip[i][j]][j-2] == 0:
-                pip[i][j+1] = pip[i][j]
-            pip[i][j] = -1
+            if pip[i][j] != -1 and deco[pip[i][j]][j - 2] == 0:
+                if j != 4:
+                    pip[i][j + 1] = pip[i][j]
+                pip[i][j] = -1
+    for i in range(2):
         if pip[i][1] != -1:
-            used = prog[pip[i][1]].split(" ")[1:]
-            if set(pip[1][2:]) == {-1}:
+            first_inst = list(set(pip[i]))
+            where_pip = list(set(pip[0] + pip[1]))
+            if set(pip[i][2:]) == {-1} and where_pip[0] == first_inst[0]:
                 pip[i][2] = pip[i][1]
                 pip[i][1] = -1
+            elif where_pip[0] != first_inst[0]:
+                if (pr[where_pip[0]].split(" ")[1] not in pr[pip[i][1]]
+                        and pr[where_pip[0]].split(" ")[2] not in pr[pip[i][1]]):
+                    pip[i][2] = pip[i][1]
+                    pip[i][1] = -1
+            elif pr[where_pip[0]].split(" ")[0] == "STORE" and pip[i][1] == where_pip[1] \
+                    and (pip[0][-1] == where_pip[0] or pip[1][-1] == where_pip[0]):
+                pip[i][2] = pip[i][1]
+                pip[i][1] = -1
+
         if pip[i][0] != -1 and pip[i][1] == -1:
             pip[i][1] = pip[i][0]
             pip[i][0] = -1
-    return pip
 
 
 def fetch(pip, pr, curs):
+    """
+
+    :param pip:
+    :param pr:
+    :param curs:
+    :return:
+    """
     for i in range(2):  # FETCH
         if curs < len(pr) - 1 and pip[i][0] == -1:
             pip[i][0] = curs
@@ -70,78 +109,125 @@ def fetch(pip, pr, curs):
 
 
 def decode(pip, pr, deco):
-    for j in range(2):  # DECODE
-        if pip[j][1] != -1:
-            deco[pip[j][1]] = CYCLES[pr[pip[j][1]].split(" ")[0]][:]
+    """
+
+    :param pip:
+    :param pr:
+    :param deco:
+    :return:
+    """
+    for i in range(2):  # DECODE
+        if pip[i][1] != -1:
+            deco[pip[i][1]] = CYCLES[pr[pip[i][1]].split(" ")[0]][:]
 
 
 def execute(rg, pip, pr, deco, v):
-    for j in range(2):  # EX
-        if pip[j][2] in deco and deco[pip[j][2]][0] > 0:
-            inst = pr[pip[j][2]].split(" ")
-            numbers = (inst[1], inst[2])
-            if inst[0] == "IADD":
-                v[j] = 0
-                for nb in numbers:
-                    v[j] += rg[REGISTER_NAME.index(nb)]
-            elif inst[0] == "IMUL" and deco[pip[j][2]][0] == 3:
-                v[j] = 1
-                for nb in numbers:
-                    v[j] *= nb
-            deco[pip[j][2]][0] -= 1
+    """
+
+    :param rg:
+    :param pip:
+    :param pr:
+    :param deco:
+    :param v:
+    :return:
+    """
+    for i in range(2):  # EX
+        if pip[i][2] in deco and deco[pip[i][2]][0] > 0:
+            inst = pr[pip[i][2]].split(" ")
+            if inst[0] == "IADD" or inst[0] == "IMUL":
+                numbers = []
+                for j in range(2):
+                    numbers.append(rg[REGISTER_NAME.index(inst[j + 1])])
+                if inst[0] == "IADD":
+                    v[i] = 0
+                    for nb in numbers:
+                        v[i] += nb
+                elif deco[pip[i][2]][0] == 3:
+                    v[i] = 1
+                    for nb in numbers:
+                        v[i] *= nb
+            deco[pip[i][2]][0] -= 1
 
 
-def memory(rg, mem, pip, pr, deco):
-    for j in range(2):  # MEM
-        if pip[j][3] in deco and deco[pip[j][3]][1] > 0:
-            inst = pr[pip[j][3]].split(" ")
-            if inst[0] == "LOAD" and deco[pip[j][3]][0] == 2:
-                value[j] = mem[int(inst[2])]
+def memory(rg, mem, pip, pr, deco, v):
+    """
+
+    :param v:
+    :param rg:
+    :param mem:
+    :param pip:
+    :param pr:
+    :param deco:
+    :return:
+    """
+    for i in range(2):  # MEM
+        if pip[i][3] in deco and deco[pip[i][3]][1] > 0:
+            inst = pr[pip[i][3]].split(" ")
+            if inst[0] == "LOAD" and deco[pip[i][3]][1] == 2:
+                v[i] = mem[int(inst[2])]
             elif inst[0] == "STORE":
-                mem[int(inst[1])] = rg[REGISTER_NAME.index(inst(2))]
-            deco[pip[j][3]][1] -= 1
+                mem[int(inst[1])] = rg[REGISTER_NAME.index(inst[2])]
+            deco[pip[i][3]][1] -= 1
 
 
-def write_back(rg, pip, v,  pr, deco):
-    for j in range(2):  # WB:
-        if pip[j][4] in deco and deco[pip[j][4]][2] > 0:
-            inst = pr[pip[j][4]].split(" ")
+def write_back(rg, pip, v, pr, deco):
+    """
+
+    :param rg:
+    :param pip:
+    :param v:
+    :param pr:
+    :param deco:
+    :return:
+    """
+    for i in range(2):  # WB:
+        if pip[i][4] in deco and deco[pip[i][4]][2] > 0:
+            inst = pr[pip[i][4]].split(" ")
             if inst[0] != "STORE":
-                if v[j] != "-1":
-                    rg[REGISTER_NAME.index(inst[1])] = v[j]
-                    v[j] = "-1"
-                else:
+                if v[i] != "-1":
+                    rg[REGISTER_NAME.index(inst[1])] = v[i]
+                    v[i] = "-1"
+                elif inst[0] == "MVC":
                     rg[REGISTER_NAME.index(inst[1])] = int(inst[2])
-            deco[pip[j][-1]][-1] -= 1
+                else:
+                    rg[REGISTER_NAME.index(inst[1])] = rg[REGISTER_NAME.index(inst[2])]
+            deco[pip[i][-1]][-1] -= 1
 
 
-if __name__ == '__main__':
-    file_name = str(input("Name of the prog > "))
+def micro_pross(name):
+    """
 
-    registres, memoires, pipelines, prog = initialise()
-    value = ["-1"]*2
+    :param name:
+    :return:
+    """
+    registres, memoires, pipelines, prog = initialise(name)
+    value = ["-1"] * 2
     first = True
     decoded = {}
-    cursor_prog, count = 0, 0
+    cursor_prog, count = 0, 1
 
-    while (set(pipelines[0]) != {-1} and set(pipelines[1]) != {-1}) or first:
+    while set(pipelines[0]) != {-1} or set(pipelines[1]) != {-1} or first:
         first = False
 
         cursor_prog = fetch(pipelines, prog, cursor_prog)
 
-        count += 1
         printState(count, pipelines[0], pipelines[1], registres, memoires)
 
         decode(pipelines, prog, decoded)
 
         execute(registres, pipelines, prog, decoded, value)
 
-        memory(registres, memoires, pipelines, prog, decoded)
+        memory(registres, memoires, pipelines, prog, decoded, value)
 
         write_back(registres, pipelines, value, prog, decoded)
 
-        print("- "*20)
+        move(pipelines, decoded, prog)
 
-        pipelines = move(pipelines, decoded, prog)
+        count += 1
+    #  Print of the end
+    printState(count, pipelines[0], pipelines[1], registres, memoires)
 
-        time.sleep(0.5)
+
+# main
+if __name__ == '__main__':
+    micro_pross(str(input("Name of the prog > ")))
