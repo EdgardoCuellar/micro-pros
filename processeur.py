ad7ab68ce62d2d52e82b3dab30faaaf85e28c69c
Edgardo610 <Edgardo610@users.noreply.github.com>
2019-12-13 17:14:57 +0100

    ITS ENDED

--- processeur.py
+++ processeur.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Projet similateur de micro-processeur
-Creation d'un processeur virtuel"ProcesseurZ8000" avec ses 10 registres et ces deux
-pipelines et ses instructions.
-Created on Tuesday November 27 14:31:11 2019
+Creation d'un processeur virtuel"ProcesseurZ8000" avec ses 10 registres, 16 cases memoires
+et deux pipelines (pipeline RISCclassique) qui permettent d'excecuter des instructions en
+simultatné. Lit les instructions provenant d'un fichier txt qu'on lui donne, et excecute
+les instructions.
+
+Created on Tuesday December 11 16:42:37 2019
 @author: Edgardo Cuellar Sanchez
 N° de matricule : 496612
 Mail : Edgardo.Cuellar.Sanchez@ulb.be
 """
-import time
 
 # GLOBAL VARIABLE
 REGISTER_NAME = ["R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9"]
@@ -22,8 +24,10 @@ CYCLES = {"LOAD": [1, 2, 1],
 
 def initialise(name):
     """
-
-    :return:
+    Simple fonction qui initialise les variables de base de notre processeur, telle que
+    la memoire, les registres, les pipelines, et lit le fichier d'instruction
+    :param name: le nom du fichier contenant les instructions
+    :return: la memoire, les registres, les pipelines et les pipelines le tout initialisé
     """
     rg = [0] * 10
     memories = [0] * 16
@@ -37,13 +41,14 @@ def initialise(name):
 
 def printState(c, p1, p2, Reg, Mem):
     """
-
-    :param c:
-    :param p1:
-    :param p2:
-    :param Reg:
-    :param Mem:
-    :return:
+    Affiche simplement nos differentes parties de notre processeur, pour en connaitre
+    l'avancement
+    :param c: le cycle actuel de notre processeur
+    :param p1: la pipeline 1
+    :param p2: la pipeline 1
+    :param Reg: les registres
+    :param Mem: la memoire
+    :return: None
     """
     print("Cycle:")
     print(c)
@@ -59,30 +64,34 @@ def printState(c, p1, p2, Reg, Mem):
 
 def move(pip, deco, pr):
     """
-
-    :param pip:
-    :param deco:
-    :param pr:
-    :return:
+    Deplace les instructions dans les pipelines dans l'ordre décroisant, et de manière a qu'il n'y
+    est pas de conflit, ou de probleme.
+    :param pip: un tableau contenant nos deux pipelines
+    :param deco: les instructions decodé en dictionnaire
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :return: None
     """
-    for i in range(2):
+    for i in range(2):  # Fait avancer les 3 dernieres instructions dans l'ordre
         for j in range(4, 1, -1):
             if pip[i][j] != -1 and deco[pip[i][j]][j - 2] == 0:
-                if j != 4:
+                if j != 4:  # Evite la dernière instruction qui ne se deplace pas mais diparait
                     pip[i][j + 1] = pip[i][j]
                 pip[i][j] = -1
-    for i in range(2):
+    for i in range(2):  # se charge de bloquer les instructions au bon moment pour eviter les bugs
         if pip[i][1] != -1:
             first_inst = list(set(pip[i]))
             where_pip = list(set(pip[0] + pip[1]))
+            # Si il n'y a rien devant qui bloque avance simplement
             if set(pip[i][2:]) == {-1} and where_pip[0] == first_inst[0]:
                 pip[i][2] = pip[i][1]
                 pip[i][1] = -1
+            # Si il y a des instructions cherche a savoire si il peut avancer ou non
             elif where_pip[0] != first_inst[0]:
                 if (pr[where_pip[0]].split(" ")[1] not in pr[pip[i][1]]
                         and pr[where_pip[0]].split(" ")[2] not in pr[pip[i][1]]):
                     pip[i][2] = pip[i][1]
                     pip[i][1] = -1
+            # Si c'est un STORE il peut donc avancer si celui ci est à la dernière case
             elif pr[where_pip[0]].split(" ")[0] == "STORE" and pip[i][1] == where_pip[1] \
                     and (pip[0][-1] == where_pip[0] or pip[1][-1] == where_pip[0]):
                 pip[i][2] = pip[i][1]
@@ -95,11 +104,11 @@ def move(pip, deco, pr):
 
 def fetch(pip, pr, curs):
     """
-
-    :param pip:
-    :param pr:
-    :param curs:
-    :return:
+    Va chercher la ligne de l'instruction et l'insere dans nos pipelines aux premieres case
+    :param pip: un tableau contenant nos deux pipelines
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :param curs: l'avancement dans les lignes de notre fichier programme
+    :return: la ligne à la quelle nous somme dans le fichier
     """
     for i in range(2):  # FETCH
         if curs < len(pr) - 1 and pip[i][0] == -1:
@@ -110,26 +119,29 @@ def fetch(pip, pr, curs):
 
 def decode(pip, pr, deco):
     """
-
-    :param pip:
-    :param pr:
-    :param deco:
-    :return:
+    Decode l'instruction grace a un tableau, et sait donc combien de cycle celle ci
+    devra passer dans chacune des parties, tout cela dans un dictionnaire
+    :param pip: un tableau contenant nos deux pipelines
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :param deco: les instructions decodé en dictionnaire
+    :return: None
     """
     for i in range(2):  # DECODE
         if pip[i][1] != -1:
             deco[pip[i][1]] = CYCLES[pr[pip[i][1]].split(" ")[0]][:]
 
 
-def execute(rg, pip, pr, deco, v):
+def execute(reg, pip, pr, deco, var):
     """
-
-    :param rg:
-    :param pip:
-    :param pr:
-    :param deco:
-    :param v:
-    :return:
+    Execute l'instruction donné si il y a besoin de l'executé, certain cycle ne servant a rien
+    et d'autres en prenant plusieurs, permet surtout les additions et les multiplication
+    :param reg: un tableau contant tout les registres de notre processeur
+    :param pip: un tableau contenant nos deux pipelines
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :param deco: les instructions decodé en dictionnaire
+    :param var: un tableau de valeur en sorties de nos deux pipelines, pour faciliter
+    l'ecriture plus loin dans le processeur
+    :return: None
     """
     for i in range(2):  # EX
         if pip[i][2] in deco and deco[pip[i][2]][0] > 0:
@@ -137,92 +149,92 @@ def execute(rg, pip, pr, deco, v):
             if inst[0] == "IADD" or inst[0] == "IMUL":
                 numbers = []
                 for j in range(2):
-                    numbers.append(rg[REGISTER_NAME.index(inst[j + 1])])
-                if inst[0] == "IADD":
-                    v[i] = 0
+                    numbers.append(reg[REGISTER_NAME.index(inst[j + 1])])
+                if inst[0] == "IADD":  # Effectue une simple addition en 1 cycle
+                    var[i] = 0
                     for nb in numbers:
-                        v[i] += nb
-                elif deco[pip[i][2]][0] == 3:
-                    v[i] = 1
+                        var[i] += nb
+                elif deco[pip[i][2]][0] == 3:  # Effectue une simple multiplication en 1 cycle
+                    var[i] = 1
                     for nb in numbers:
-                        v[i] *= nb
+                        var[i] *= nb
             deco[pip[i][2]][0] -= 1
 
 
-def memory(rg, mem, pip, pr, deco, v):
+def memory(reg, mem, pip, pr, deco, var):
     """
-
-    :param v:
-    :param rg:
-    :param mem:
-    :param pip:
-    :param pr:
-    :param deco:
-    :return:
+    Gére la parties des instructions fesant appelle à la mémoires, écrit et lit les informations
+    dans celle ci
+    :param reg: un tableau contant tout les registres de notre processeur
+    :param mem: un tableau contant tout la mémoire de notre processeur
+    :param pip: un tableau contenant nos deux pipelines
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :param deco: les instructions decodé en dictionnaire
+    :param var: un tableau de valeur en sorties de nos deux pipelines, pour faciliter
+    l'ecriture plus loin dans le processeur
+    :return: None
     """
     for i in range(2):  # MEM
         if pip[i][3] in deco and deco[pip[i][3]][1] > 0:
             inst = pr[pip[i][3]].split(" ")
-            if inst[0] == "LOAD" and deco[pip[i][3]][1] == 2:
-                v[i] = mem[int(inst[2])]
-            elif inst[0] == "STORE":
-                mem[int(inst[1])] = rg[REGISTER_NAME.index(inst[2])]
+            if inst[0] == "LOAD" and deco[pip[i][3]][1] == 2:  # Lit la mémoire et stocke la donnée
+                var[i] = mem[int(inst[2])]
+            elif inst[0] == "STORE":  # Ecrit dans la mémoire depuis un registre
+                mem[int(inst[1])] = reg[REGISTER_NAME.index(inst[2])]
             deco[pip[i][3]][1] -= 1
 
 
-def write_back(rg, pip, v, pr, deco):
+def write_back(reg, pip, var, pr, deco):
     """
-
-    :param rg:
-    :param pip:
-    :param v:
-    :param pr:
-    :param deco:
-    :return:
+    Gére les registres, permet d'ecrire les valeurs dans les registres
+    :param reg: un tableau contant tout les registres de notre processeur
+    :param pip: un tableau contenant nos deux pipelines
+    :param var: un tableau de valeur en sorties de nos deux pipelines, pour faciliter
+    l'ecriture plus loin dans le processeur
+    :param pr: les instructions du programme decoupé en tableau ligne par ligne
+    :param deco: les instructions decodé en dictionnaire
+    :return: None
     """
     for i in range(2):  # WB:
         if pip[i][4] in deco and deco[pip[i][4]][2] > 0:
             inst = pr[pip[i][4]].split(" ")
             if inst[0] != "STORE":
-                if v[i] != "-1":
-                    rg[REGISTER_NAME.index(inst[1])] = v[i]
-                    v[i] = "-1"
-                elif inst[0] == "MVC":
-                    rg[REGISTER_NAME.index(inst[1])] = int(inst[2])
-                else:
-                    rg[REGISTER_NAME.index(inst[1])] = rg[REGISTER_NAME.index(inst[2])]
+                if var[i] != "-1":  # Ecrit la valeur recupéré plus tot dans un registre
+                    reg[REGISTER_NAME.index(inst[1])] = var[i]
+                    var[i] = "-1"
+                elif inst[0] == "MVC":  # Ecrit le nombre donnée dans la mémoire
+                    reg[REGISTER_NAME.index(inst[1])] = int(inst[2])
+                else:  # Copie un registre dans un autre
+                    reg[REGISTER_NAME.index(inst[1])] = reg[REGISTER_NAME.index(inst[2])]
             deco[pip[i][-1]][-1] -= 1
 
 
 def micro_pross(name):
     """
-
-    :param name:
-    :return:
+    La fonction principale de notre programme qui permet a tout le processeur de marcher
+    en fesant appelle au différentre fonction de notre programme
+    :param name: le nom du fichier contenant nos instructions
+    :return: None
     """
     registres, memoires, pipelines, prog = initialise(name)
-    value = ["-1"] * 2
     first = True
+    value = ["-1"] * 2
     decoded = {}
     cursor_prog, count = 0, 1
 
     while set(pipelines[0]) != {-1} or set(pipelines[1]) != {-1} or first:
         first = False
 
-        cursor_prog = fetch(pipelines, prog, cursor_prog)
+        cursor_prog = fetch(pipelines, prog, cursor_prog)  # IF
 
         printState(count, pipelines[0], pipelines[1], registres, memoires)
 
-        decode(pipelines, prog, decoded)
-
-        execute(registres, pipelines, prog, decoded, value)
-
-        memory(registres, memoires, pipelines, prog, decoded, value)
-
-        write_back(registres, pipelines, value, prog, decoded)
+        decode(pipelines, prog, decoded)  # ID
+        execute(registres, pipelines, prog, decoded, value)  # EX
+        memory(registres, memoires, pipelines, prog, decoded, value)  # MEM
+        write_back(registres, pipelines, value, prog, decoded)  # WB
 
         move(pipelines, decoded, prog)
-
         count += 1
     #  Print of the end
     printState(count, pipelines[0], pipelines[1], registres, memoires)
